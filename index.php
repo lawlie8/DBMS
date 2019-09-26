ef959547eb0f7d56c44ff21656dac6c5a0bc69b6
lawlie8 <lawlie8@pm.me>
2019-09-27 01:02:28 +0530

    /

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" onload="document.body.style.zoom = 1.0">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<link rel="stylesheet" href="css/master.css">
+  <head>
+    <meta charset="utf-8">
+<title>Ariayaz Travels</title>
+  </head>
+
+<div class="nav_bar">
+    <div class="nav_elements">
+  <li><a href="index.php">Home</a></li>
+  <li><a href="Books.php">All Accomodation</a></li>
+  <li><a href="Game.php">Flights</a></li>
+  <li><a href="Series.php">Car Rentals</a></li>
+
+<li>
+
+<div class="dropdown">
+  <button type="button" class="drp_btn">Other</button>
+<div class="dropdown_content">
+<a  href="">Steam Link</a>
+<a  href="About.php">About us</a>
+</div>
+</div>
+</li>
+
+<div class="register_section">
+  <li><a href="#">Login/Signup</a></li>
+
+</div>
+</div>
+
+</div>
+<!--////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+<div class="main">
+<li><input type="text" name="" value=""  placeholder="   Where are you going"></li>
+<li><input placeholder="   Date" width='' class="textbox-n" type="text" onfocus="(this.type='date')"   onblur="(this.type='text')" id="date"> </li>
+<li><input type="text" onfocus="this.type='range'" onblur="this.type='text'" placeholder="   Number of passengers" name="" value=""> </li>
+
+</div>
